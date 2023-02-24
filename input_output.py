8fac7498e9765ad65bdb21c0abe94349f9c3f68e
Tiago Rodrigues <tpr09756@gmail.com>
2023-02-24 11:28:21 -0100

    init

---
+++ input_output.py
@@ -0,0 +1,5 @@
+num1 = input("Please enter a number: ")
+num2 = input("Please enter a second number: ")
+
+print (type(num1))
+print(int(num1) + int(num2))
---
+++ python_variables.py
@@ -0,0 +1,10 @@
+a = b = c = 10
+
+print(a)
+print(b)
+print(c)
+
+x,y,z = 1,2,3
+print(x)
+print(y)
+print(z)
